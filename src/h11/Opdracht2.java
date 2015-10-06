e0abc46b56258217c9c2b6c0ebde8bbd2c56f36a
David <David@10.68.172.26>
2015-10-06 11:54:19 +0200

    h11

---
+++ src/h11/Opdracht2.java
@@ -0,0 +1,22 @@
+package h11; 
+ 
+ 
+ import java.applet.Applet; 
+ import java.awt.Graphics; 
+ 
+ 
+ @SuppressWarnings("serial") 
+ public class Opdracht2 extends Applet{ 
+ 	public void paint(Graphics g)
+ 	{ 
+ 		int i = 10; 
+ 		int x = 50; 
+ 		int y = 50; 
+ 		while (i < 21)
+ 		{  
+			g.drawString(""+i, x, y); 
+ 			y += 20; 
+			i++; 
+ 		}  
+ 	} 
+} 
