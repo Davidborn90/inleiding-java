1a0ee2186d9d627419b4ed845ba2e6903bc4478b
David <David@10.68.172.26>
2015-10-30 10:53:43 +0100

    h12

---
+++ src/h12/Opdracht2.java
@@ -0,0 +1,19 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.Button;
+
+@SuppressWarnings("serial")
+public class Opdracht2 extends Applet {
+	
+	Button[]knoppen = new Button[25];
+	
+	public void init() {
+		setSize(400, 150);
+		for (int i = 0; i < knoppen.length; i++) {
+			knoppen[i] = new Button("knop #"+ (i+1));
+			add(knoppen[i]);
+		}
+	}
+	
+}
